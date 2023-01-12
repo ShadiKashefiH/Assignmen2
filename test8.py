d02d1a0d0f5c58f704421295e69064bdb6771886
ShadiKashefiH <122170938+ShadiKashefiH@users.noreply.github.com>
2023-01-12 14:38:45 +0330

    Add files via upload

--- test8.py
+++ test8.py
@@ -2,7 +2,7 @@ a= float(input("Please enter a="))
 b= float(input("Please enter b="))
 c= float(input("Please enter c="))
 
-if a+b>c:
+if a+b>c and a+c>b and b+c>a:
     result= print("It is a triangle.")
 else:
     result= print("It is not a triangle.")
